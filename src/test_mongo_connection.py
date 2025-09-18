1b64e7d653ebe74c900166c627bd5e56741778cc
cjayneb <70963776+cjayneb@users.noreply.github.com>
2025-09-18 14:06:03 -0400

    add back auth and add seed data manually

--- src/test_mongo_connection.py
+++ src/test_mongo_connection.py
@@ -1,8 +1,11 @@
 from pymongo import MongoClient
 
+from models.user import User
+
 def test_mongo_connection():
-    client = MongoClient("mongodb://@127.0.0.1:27017/")
+    client = MongoClient("mongodb://user:pass@127.0.0.1:27017/")
     db = client["store_manager"]
+    db["users"].insert_one(User(None, "name", "email").__dict__)
     users = list(db["users"].find({}))
-    assert len(users) >= 2
+    assert len(users) >= 1
     print("MongoDB connection successful, users found:", users)
