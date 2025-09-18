35dacfc72e929a93ba8cf11c0827c6bffa27cb04
cjayneb <70963776+cjayneb@users.noreply.github.com>
2025-09-18 13:54:48 -0400

    try fix again

--- src/test_mongo_connection.py
+++ src/test_mongo_connection.py
@@ -3,6 +3,6 @@ from pymongo import MongoClient
 def test_mongo_connection():
     client = MongoClient("mongodb://user:pass@127.0.0.1:27017/")
     db = client["store_manager"]
-    users = list(db["users"].find())
+    users = list(db["users"].find({}))
     assert len(users) >= 2
     print("MongoDB connection successful, users found:", users)
