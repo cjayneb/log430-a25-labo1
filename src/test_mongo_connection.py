65d5ebe193917336f447efd89059d37b0c4928a8
cjayneb <70963776+cjayneb@users.noreply.github.com>
2025-09-18 14:01:03 -0400

    remove auth

--- src/test_mongo_connection.py
+++ src/test_mongo_connection.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 def test_mongo_connection():
-    client = MongoClient("mongodb://user:pass@127.0.0.1:27017/")
+    client = MongoClient("mongodb://@127.0.0.1:27017/")
     db = client["store_manager"]
     users = list(db["users"].find({}))
     assert len(users) >= 2
