cc18d4da4945207ce57e332288422a7ccd9c9c81
cjayneb <70963776+cjayneb@users.noreply.github.com>
2025-09-18 14:10:03 -0400

    put mongo init in folder

--- src/test_mongo_connection.py
+++ src/test_mongo_connection.py
@@ -5,7 +5,7 @@ from models.user import User
 def test_mongo_connection():
     client = MongoClient("mongodb://user:pass@127.0.0.1:27017/")
     db = client["store_manager"]
-    db["users"].insert_one(User(None, "name", "email").__dict__)
+    #db["users"].insert_one(User(None, "name", "email").__dict__)
     users = list(db["users"].find({}))
     assert len(users) >= 1
     print("MongoDB connection successful, users found:", users)
